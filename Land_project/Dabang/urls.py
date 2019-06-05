f04ff9e0e03d5d44c9b15e16fe40482eeebeb3d7
sug5806 <sug5806@gmail.com>
2019-06-05 20:04:35 +0900

    feat: uncomplete pass data

--- Land_project/Dabang/urls.py
+++ Land_project/Dabang/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
 
+app_name = 'dabang'
 
 urlpatterns = [
     path('', Dabang_List.as_view(), name='list'),
+    # path('search/', Dabang_search.as_view(), name='search'),
+    path('search/', dabang_Search, name='search'),
 ]
--- Land_project/Dabang/views.py
+++ Land_project/Dabang/views.py
@@ -3,13 +3,16 @@ import sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
 
 from django.views.generic import ListView
+from django.views import View
+from django.shortcuts import render, render_to_response
 from .models import *
 from .secret import Public_data, KakaoAPI
 
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus
-from pprint import pprint
 import xmltodict, json
+
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -23,6 +26,24 @@ class Dabang_List(ListView):
         return context
 
 
+
+
+class Dabang_search(View):
+    pass
+
+def dabang_Search(request):
+    if request.method == "GET":
+        return render_to_response('Dabang/search_list.html')
+
+
+    if request.is_ajax():
+        data = request.POST
+
+        # return render(request,'Dabang/search_list.html',)
+        return HttpResponse("OK")
+
+
+
 def get_info():
     # serviceKey = 'iW7CmOM8mzRIMTwitueEeqdoxZm7NVxjMnHA7+8j8T3aTt+J3DGYbMVRjcJhX7j5D7O7sg7AWG7m1RE14GmbvA=='
 
@@ -30,7 +51,7 @@ def get_info():
     queryParams = '?' + urlencode({
         quote_plus('ServiceKey'): Public_data,
         quote_plus('pageNo'): '1',
-        quote_plus('numOfRows'): '10',
+        quote_plus('numOfRows'): '20',
         quote_plus('LAWD_CD'): '11110',
         quote_plus('DEAL_YMD'): '201512'})
 
@@ -43,8 +64,3 @@ def get_info():
     json_type = json.dumps(dict_type)
     dict_type2 = json.loads(json_type)
     print(dict_type2)
-
-
-
-
-get_info()

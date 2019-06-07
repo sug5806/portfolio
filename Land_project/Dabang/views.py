bb6182f9845ed147c220811f4b5922dac94517c6
sug5806 <sug5806@gmail.com>
2019-06-07 16:40:29 +0900

    feat: success pass the data using ajax to view

--- Land_project/Dabang/views.py
+++ Land_project/Dabang/views.py
@@ -31,20 +31,41 @@ class Dabang_List(ListView):
 class Dabang_search(View):
     pass
 
+from django.http import JsonResponse
+
 def dabang_Search(request):
     if request.method == "GET":
-        return render_to_response('Dabang/search_list.html')
+        data = request.GET
+        print(data)
+
+        # print(data['addr[address]'])
+        # print(data["addr[zonecode]"])
+        add = data['addr[address]']
+        zone = data["addr[sigunguCode]"]
+
+        print(add, zone)
 
 
-    if request.is_ajax():
-        data = request.POST
+        context = {
+            'address': add,
+            'zonecode': zone,
+        }
 
-        # return render(request,'Dabang/search_list.html',)
-        return HttpResponse("OK")
+        print()
+        print()
+        print()
 
+        context.update(get_info(zone))
 
+        # return render(request, 'Dabang/search_list.html', context)
+        # return render(request, 'Dabang/search_list.html')
+        return JsonResponse(context)
 
-def get_info():
+from pprint import pprint
+
+def get_info(LAW):
+    context = dict()
+
     # serviceKey = 'iW7CmOM8mzRIMTwitueEeqdoxZm7NVxjMnHA7+8j8T3aTt+J3DGYbMVRjcJhX7j5D7O7sg7AWG7m1RE14GmbvA=='
 
     url = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev'
@@ -52,8 +73,9 @@ def get_info():
         quote_plus('ServiceKey'): Public_data,
         quote_plus('pageNo'): '1',
         quote_plus('numOfRows'): '20',
-        quote_plus('LAWD_CD'): '11110',
+        quote_plus('LAWD_CD'): LAW,
         quote_plus('DEAL_YMD'): '201512'})
+
 
     request = Request(url + queryParams)
 
@@ -63,4 +85,29 @@ def get_info():
     dict_type = xmltodict.parse(response_body)
     json_type = json.dumps(dict_type)
     dict_type2 = json.loads(json_type)
-    print(dict_type2)
+
+
+
+    price = dict_type2['response']['body']['items']['item'][0]['거래금액']
+    name = dict_type2['response']['body']['items']['item'][0]['아파트']
+    year = dict_type2['response']['body']['items']['item'][0]['년']
+    month = dict_type2['response']['body']['items']['item'][0]['월']
+    day = dict_type2['response']['body']['items']['item'][0]['일']
+    floor = dict_type2['response']['body']['items']['item'][0]['층']
+
+    context.update(
+        price=dict_type2['response']['body']['items']['item'][0]['거래금액'],
+        name=dict_type2['response']['body']['items']['item'][0]['아파트'],
+        year=year,
+        month=month,
+        day=day,
+        floor=floor,
+    )
+
+    pprint(dict_type2['response']['body']['items']['item'][0]['거래금액'])
+    print()
+    print()
+    print()
+    print()
+    return context
+

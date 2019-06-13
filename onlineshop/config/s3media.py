cb1c5b4643ba10350ddbb3cf2961d9d834dd929d
sug5806 <sug5806@gmail.com>
2019-06-13 11:55:02 +0900

    feat: delete onlineshop

--- onlineshop/config/s3media.py
+++
@@ -1,8 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-
-class MediaStorage(S3Boto3Storage):
-    location = ''
-    bucket_name = 'm-shop'
-    region_name = 'ap-northeast-2'
-    custom_domain = '%s.s3.%s.amazonaws.com' % (bucket_name, region_name)
-    file_overwrite = False
